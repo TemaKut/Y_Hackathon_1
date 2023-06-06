7dac4721e2e4c0aeec841bffac5d7a91bbe022b2
Dmitriy Ushakov Air <udv8@ya.ru>
2023-06-06 23:57:20 +0300

    B: add endpoints order and products

--- backend/app/core/orders/schemas.py
+++ backend/app/core/orders/schemas.py
@@ -1,1 +1,31 @@
-# from pydantic import BaseModel
+from typing import List
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    username: str
+
+
+class Order(BaseModel):
+    box_num: str
+    goods_wght: str
+    orderkey: str
+    pack_volume: str
+    rec_calc_cube: str
+    recommended_carton: str
+    sel_calc_cube: str
+    selected_carton: str
+    sku: str
+    trackingid: str
+    who: str
+    whs: str
+
+
+class Product(BaseModel):
+    sku: str
+    logo: str
+    name: str
+    cargotypes: List[int]
+    additionalPackaging: str
+    userTip: str
