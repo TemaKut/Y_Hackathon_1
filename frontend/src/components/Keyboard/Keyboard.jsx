52607901fd58a2c68749e5a076abdc2ea810aa29
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-06-14 12:19:01 +0200

    refactor: move number buttons container to a separate component

--- frontend/src/components/Keyboard/Keyboard.jsx
+++ frontend/src/components/Keyboard/Keyboard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // import ReactPortal from '../ReactPortal/ReactPortal';
-import buttons from './buttons';
 import Style from './Keyboard.module.scss';
+import NumberButtons from './NumberButtons/NumberButtons';
 
-function Keyboard() {
+function Keyboard({ children }) {
   // const [isModalOpen, setIsModalOpen] = useState(true);
   // const closeModal = () => setIsModalOpen(false);
 
@@ -14,33 +14,17 @@ function Keyboard() {
     // <ReactPortal wrapperId="modal-container">
     // <section className={`${Style.modal} ${isModalOpen && Style.openModal}`}>
     <section className={Style.keyboard}>
-      {/* <div className={Style.content}>{children}</div> */}
       <h2 className={Style.title}>Введите штрихкод товара</h2>
       <input className={Style.input} />
-      <div className={Style.buttons}>
-        {buttons.map((button) => (
-          <button
-            className={`${Style.button} ${Style[`button_${button.name}`]}`}
-            key={button.id}
-            type="button"
-            aria-label={button.name}
-          >
-            {button.value}
-          </button>
-        ))}
-        <button
-          className={`${Style.button} ${Style.button_delete}`}
-          type="button"
-          aria-label="Удалить символ"
-        />
-      </div>
+      {children}
+      <NumberButtons />
     </section>
     // </ReactPortal>
   );
 }
 
-// Keyboard.propTypes = {
-//   children: PropTypes.node.isRequired,
-// };
+Keyboard.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Keyboard;
