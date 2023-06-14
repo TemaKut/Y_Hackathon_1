26bbcff4eda92371ca3a312a1a75515e8c169bd2
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-06-14 12:09:53 +0200

    feat: button keyboard styling is finished

--- frontend/src/components/Keyboard/Keyboard.jsx
+++ frontend/src/components/Keyboard/Keyboard.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 // import ReactPortal from '../ReactPortal/ReactPortal';
-// import closeIcon from '../../images/close-icon.svg';
 import buttons from './buttons';
 import Style from './Keyboard.module.scss';
 
@@ -18,22 +17,23 @@ function Keyboard() {
       {/* <div className={Style.content}>{children}</div> */}
       <h2 className={Style.title}>Введите штрихкод товара</h2>
       <input className={Style.input} />
-      <ul className={Style.buttons}>
-        {buttons.map((item) => (
-          <li className={Style.buttonContainer}>
-            <button className={Style.button} type="button" aria-label={item}>
-              {item}
-            </button>
-          </li>
-        ))}
-        <li className={Style.buttonContainer}>
+      <div className={Style.buttons}>
+        {buttons.map((button) => (
           <button
-            className={Style.button}
+            className={`${Style.button} ${Style[`button_${button.name}`]}`}
+            key={button.id}
             type="button"
-            aria-label="Удалить символ"
-          />
-        </li>
-      </ul>
+            aria-label={button.name}
+          >
+            {button.value}
+          </button>
+        ))}
+        <button
+          className={`${Style.button} ${Style.button_delete}`}
+          type="button"
+          aria-label="Удалить символ"
+        />
+      </div>
     </section>
     // </ReactPortal>
   );
