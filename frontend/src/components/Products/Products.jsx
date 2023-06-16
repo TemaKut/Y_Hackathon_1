78fc89058128ec6ef077efbc62f25805e878147c
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-06-16 17:54:49 +0200

    refactor: add Button component to the Products section

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Style from './Products.module.scss';
+import Button from '../UI/Button/Button';
 import Cell from '../Cell/Cell';
 import Card from './Card/Card';
 
 function Products() {
   return (
-    <>
+    <section className={Style.products}>
+      <Button
+        // onClickBtn={handleClickBtn}
+        btnPosition="left"
+        btnColor="grey"
+        btnSize="big"
+        ariaLabelText="Есть проблема"
+      >
+        Есть проблема
+      </Button>
       <Cell />
-      <div className={Style.products}>
+      <div className={Style.productsContainer}>
         <div className={Style.specs}>
           <span className={`${Style.amount} ${Style.spec}`}>6 товаров</span>
           <span className={`${Style.delivery} ${Style.spec}`}>
@@ -18,7 +28,19 @@ function Products() {
           <Card />
         </ul>
       </div>
-    </>
+      <Button
+        // onClickBtn={handleClickBtn}
+        // isHidden will be updated with logic
+        isHidden
+        btnPosition="right"
+        btnColor="yellow"
+        btnSize="big"
+        isSubmit
+        ariaLabelText="Подобрать упаковку"
+      >
+        Подобрать упаковку
+      </Button>
+    </section>
   );
 }
 
--- frontend/src/components/UI/Button/Button.jsx
+++ frontend/src/components/UI/Button/Button.jsx
@@ -4,6 +4,7 @@ import Style from './Button.module.scss';
 
 function Button({
   onClickBtn,
+  isHidden,
   btnPosition,
   btnColor,
   btnSize,
@@ -18,9 +19,11 @@ function Button({
 
   return (
     <button
-      className={`${Style.button} ${Style[`button__position_${btnPosition}`]} ${
-        Style[`button__color_${btnColor}`]
-      } ${Style[`button__size_${btnSize}`]}`}
+      className={`${Style.button} ${isHidden && Style.button_hidden} ${
+        Style[`button__position_${btnPosition}`]
+      } ${Style[`button__color_${btnColor}`]} ${
+        Style[`button__size_${btnSize}`]
+      }`}
       type={isSubmit ? 'submit' : 'button'}
       onClick={isSubmit ? null : handleClickBtn}
       aria-label={ariaLabelText}
@@ -31,17 +34,19 @@ function Button({
 }
 
 Button.propTypes = {
+  onClickBtn: PropTypes.func.isRequired,
+  isHidden: PropTypes.bool,
   btnPosition: PropTypes.string.isRequired,
   btnColor: PropTypes.string.isRequired,
   btnSize: PropTypes.string.isRequired,
   isSubmit: PropTypes.bool,
   children: PropTypes.node.isRequired,
-  onClickBtn: PropTypes.func.isRequired,
   ariaLabelText: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
   isSubmit: false,
+  isHidden: false,
 };
 
 export default Button;
