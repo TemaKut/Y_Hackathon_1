a99d7962c5c92bd25fc41c3181eb82c1e38225ae
Kanapin Marat <kanapinmarat@yandex.ru>
2023-06-17 12:47:20 +0300

    added animation popup

--- frontend/src/components/Popup/Popup.jsx
+++ frontend/src/components/Popup/Popup.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import ReactPortal from '../ReactPortal/ReactPortal';
+import ReactPortal from '../ReactPortal/ReactPortal';
 import Style from './Popup.module.scss';
-import happycarton from '../../images/happycarton.png';
+// import happycarton from '../../images/happycarton.png';
 
 function Popup({ isPopupOpen }) {
   if (!isPopupOpen) return null;
 
   return (
-    // <ReactPortal wrapperId="Popup-container">
-    <div className={`${Style.popup} ${isPopupOpen}`}>
-      <section className={Style.container}>
-        <h2 className={Style.title}>Спасибо за информацию!</h2>
-        <p className={Style.text}>Подбираю другой вариант упаковки</p>
-        <img
-          className={Style.image}
-          src={happycarton}
-          alt="рисунок позитивной коробки"
-        />
-        {/* <form>
+    <ReactPortal wrapperId="Popup-container">
+      <div className={`${Style.popup} ${isPopupOpen}`}>
+        <section className={Style.container}>
+          <h2 className={Style.title}>Спасибо за информацию!</h2>
+          <p className={Style.text}>Подбираю другой вариант упаковки</p>
+          {/* <img
+            className={Style.image}
+            src={happycarton}
+            alt="рисунок позитивной коробки"
+          /> */}
+          <div className={Style.image} />
+          {/* <form>
           <input className={Style.input} />
           {children}
         </form> */}
-      </section>
-    </div>
-    // </ReactPortal>
+        </section>
+      </div>
+    </ReactPortal>
   );
 }
 
