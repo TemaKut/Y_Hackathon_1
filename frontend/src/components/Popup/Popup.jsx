d63ffc699654c9ef7902e1972d7e97d54654cf29
Kanapin Marat <kanapinmarat@yandex.ru>
2023-06-17 13:57:51 +0300

    refactor popup and getOrderProduct

--- frontend/src/components/Popup/Popup.jsx
+++ frontend/src/components/Popup/Popup.jsx
@@ -2,29 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactPortal from '../ReactPortal/ReactPortal';
 import Style from './Popup.module.scss';
-// import happycarton from '../../images/happycarton.png';
 
 function Popup({ isPopupOpen }) {
   if (!isPopupOpen) return null;
 
   return (
     <ReactPortal wrapperId="Popup-container">
-      <div className={`${Style.popup} ${isPopupOpen}`}>
-        <section className={Style.container}>
+      <section className={`${Style.popup} ${isPopupOpen}`}>
+        <div className={Style.container}>
           <h2 className={Style.title}>Спасибо за информацию!</h2>
           <p className={Style.text}>Подбираю другой вариант упаковки</p>
-          {/* <img
-            className={Style.image}
-            src={happycarton}
-            alt="рисунок позитивной коробки"
-          /> */}
           <div className={Style.image} />
-          {/* <form>
-          <input className={Style.input} />
-          {children}
-        </form> */}
-        </section>
-      </div>
+        </div>
+      </section>
     </ReactPortal>
   );
 }
