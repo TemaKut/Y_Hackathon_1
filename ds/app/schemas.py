d2e6fe80831cab76bc39999ec92d32bccfd0075d
Artem Kutuzov <tema.kutuzzzov@yandex.ru>
2023-06-17 16:08:25 +0300

    1

--- ds/app/schemas.py
+++ ds/app/schemas.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 class ProductToPredict(BaseModel):
     """ Схема данных продукта перед предсказанием модели. """
 
-<<<<<<< HEAD
     sku: str
     count: int
     a: float
@@ -12,24 +11,3 @@ class ProductToPredict(BaseModel):
     c: float
     goods_wght: float
     cargotypes: list[float]
-
-
-class OrderFromBackend(BaseModel):
-    """ Схема получаемых данных от бэка (Заказ). """
-
-=======
-    a: float
-    b: float
-    box_num: int = None
-    c: float
-    cargotypes: list[float]
-    cell_name: str = None
-    count: int
-    goods_wght: float
->>>>>>> develop
-    orderkey: str
-    pack_volume: int
-    rec_calc_cube: int
-    sel_calc_cube: int
-    sku: str = None
-    trackingid: str = None
