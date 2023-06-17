9d262194bf4952abdd28cceb4b74fde162e67271
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-06-17 15:34:52 +0200

    Feat/starting page (#6)

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import Style from './Main.module.scss';
+import getOrderCell from '../../utils/getOrderCell';
 import Cell from '../Cell/Cell';
 import Button from '../UI/Button/Button';
 
-function Main() {
+function Main({ cellName, setCellName }) {
+  const handleClickBtn = async () => {
+    const newValue = await getOrderCell();
+    setCellName(newValue.cell);
+  };
+
   return (
-    <main className={Style.main}>
-      <Cell />
+    <section className={Style.main}>
+      <Cell cellName={cellName} />
       <Button
-        // onClickBtn={handleClickBtn}
+        onClickBtn={handleClickBtn}
         btnPosition="right"
         btnColor="yellow"
         btnSize="big"
@@ -18,8 +24,17 @@ function Main() {
       >
         Взять другое задание
       </Button>
-    </main>
+    </section>
   );
 }
+
+Main.propTypes = {
+  cellName: PropTypes.string,
+  setCellName: PropTypes.func.isRequired,
+};
+
+Main.defaultProps = {
+  cellName: '',
+};
 
 export default Main;
