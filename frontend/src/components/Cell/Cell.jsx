515d8421da859dc2d920333dca1d23a022887628
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-06-17 11:43:21 +0200

    Feat/starting page (#4)

--- frontend/src/components/Cell/Cell.jsx
+++ frontend/src/components/Cell/Cell.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import useAsync from '../../utils/useAsync';
+import getOrderCell from '../../utils/getOrderCell';
 import Style from './Cell.module.scss';
 
 function Cell() {
   const location = useLocation();
   const isStart = ['/'].includes(location.pathname);
   const cellText = isStart ? 'Сканируйте ячейку' : 'Сканируйте товары';
-  const cellTitleStyle = isStart ? Style.start : '';
+  const [cellName, setCellName] = useState('');
+  const { value } = useAsync(getOrderCell);
 
-  return (
-    <h2 className={`${Style.cellTitle} ${cellTitleStyle}`}>
-      {cellText} <p className={Style.cellNumber}>B-09</p>
+  useEffect(() => {
+    if (value) {
+      setCellName(value.cell);
+    }
+  }, [value]);
+
+  return cellName ? (
+    <h2 className={`${Style.title} ${isStart && Style.start}`}>
+      {cellText} <span className={Style.cellName}>{cellName}</span>
     </h2>
-  );
+  ) : null;
 }
 
 export default Cell;
