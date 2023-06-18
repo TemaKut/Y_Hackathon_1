167d6a6d176a630b80b394b4089ca64c4feb665c
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-06-18 14:03:11 +0200

    Feat/package (#9)

---
+++ frontend/src/components/Package/packageData.js
@@ -0,0 +1,55 @@
+const packageResult = {
+  s: 'YMA',
+  m: 'YMF',
+  l: 'MYF',
+};
+
+const packageData = [
+  {
+    name: 'YMA',
+    type: 'box',
+    backgroundColor: '#FF0000',
+  },
+  {
+    name: 'YMF',
+    type: 'box',
+    backgroundColor: '#F29A00',
+  },
+  {
+    name: 'MYF',
+    type: 'box',
+    backgroundColor: '#8A35B3',
+  },
+  {
+    name: 'YMC',
+    type: 'box',
+    backgroundColor: '#16523B',
+  },
+  {
+    name: 'MYA',
+    type: 'bag',
+    backgroundColor: '#F73E47',
+  },
+  {
+    name: 'MYB',
+    type: 'bag',
+    backgroundColor: '#A7070F',
+  },
+  {
+    name: 'MYC',
+    type: 'bag',
+    backgroundColor: '#25ADD6',
+  },
+  {
+    name: 'MYD',
+    type: 'bag',
+    backgroundColor: '#F5C401',
+  },
+  {
+    name: 'MYE',
+    type: 'bag',
+    backgroundColor: '#5D4A00',
+  },
+];
+
+export default { packageResult, packageData };
